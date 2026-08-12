113ab179037f31db5d150c98efaed4891cfe358a
Wheelers-Media <nathan@wheelersmedia.ca>
2026-08-12 13:56:21 -0600

    Bump V10 map bootstrap cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='stormlens-v10-map-core-20260812-1';
+const CACHE='stormlens-v10-map-core-20260812-2';
 const APP=['./','./index.html','./styles.css','./desktop.css','./map-v6.css','./map-core-v10.css','./app.js','./radar-fix.js','./map-core-v10.js','./map-v10-bridge.js','./maptiler-env.js','./premium-home.js','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
